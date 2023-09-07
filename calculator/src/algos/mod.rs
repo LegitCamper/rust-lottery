657a6f6dc2fbe2e48b19e7ab3b538a2c36cf721f
sawyer bristol <sawyerbristol@gmail.com>
2023-09-07 16:01:45 -0600

    made optimizations way quicker

--- calculator/src/algos/mod.rs
+++ calculator/src/algos/mod.rs
@@ -1,39 +1,51 @@
 pub mod friends;
 use crate::{LotteryNumbers, LotteryTicket};
 use itertools::Itertools;
-use std::slice::Windows;
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    slice::Windows,
+    sync::Mutex,
+    {collections::HashMap, sync::Arc},
+};
+use tokio::task::spawn;
 
 type Tickets = Vec<Vec<i8>>;
 
-pub fn optimize(
+pub async fn optimize(
     numbers: Arc<LotteryNumbers>,
     ticket_size: i32,
     algo: fn(Windows<'_, LotteryTicket>, i32) -> Vec<i8>,
 ) -> Vec<i8> {
-    let mut results: HashMap<i32, i32> = HashMap::new();
+    let results: Arc<Mutex<HashMap<i32, i32>>> = Arc::new(Mutex::new(HashMap::new()));
+    // let mut task_handles: Vec<JoinHandle<()>> = Vec::new();
     for w in 1..1000 {
-        // finds the best window size that gives the best results
-        let window = numbers.windows(w);
+        let numbers = numbers.clone();
+        let results = results.clone();
+        spawn(async move {
+            // finds the best window size that gives the best results
+            let window = numbers.windows(w);
 
-        let tickets = algo(window, ticket_size);
-        let next_ticket = &numbers[w as usize].numbers;
+            let tickets = algo(window, ticket_size);
+            let next_ticket = &numbers[w as usize].numbers;
 
-        let mut matching_numbers = 0;
-        for ticket in tickets.iter().combinations(numbers[0].numbers.len()) {
-            matching_numbers += ticket
-                .iter()
-                .zip(next_ticket.clone())
-                .filter(|&(a, b)| a == &&b)
-                .count();
-        }
-        results.insert(w.try_into().unwrap(), matching_numbers.try_into().unwrap());
+            let mut matching_numbers = 0;
+            for ticket in tickets.iter().combinations(numbers[0].numbers.len()) {
+                matching_numbers += ticket
+                    .iter()
+                    .zip(next_ticket.clone())
+                    .filter(|&(a, b)| a == &&b)
+                    .count();
+            }
+            results
+                .lock()
+                .unwrap()
+                .insert(w.try_into().unwrap(), matching_numbers.try_into().unwrap());
+        });
     }
 
     let mut most_numbers = (1, 0);
-    for item in results {
-        if item.1 > most_numbers.1 {
-            most_numbers = item
+    for item in results.lock().unwrap().iter() {
+        if item.1 > &most_numbers.1 {
+            most_numbers = (*item.0, *item.1)
         }
     }
 
--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -5,20 +5,25 @@ use crate::{
 mod algos;
 mod read;
 use std::sync::Arc;
+use tokio;
 
 type LotteryNumbers = Vec<LotteryTicket>;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let numbers = Arc::new(data_keymap().unwrap());
     let ticket_length = numbers[0].numbers.len();
 
     let mut ticket_numbers: Vec<i8> = Vec::new();
 
-    ticket_numbers.append(&mut optimize(
-        numbers.clone(),
-        ticket_length.try_into().unwrap(),
-        friends::friends,
-    ));
+    ticket_numbers.append(
+        &mut optimize(
+            numbers.clone(),
+            ticket_length.try_into().unwrap(),
+            friends::friends,
+        )
+        .await,
+    );
 
     let tickets = get_tickets(ticket_numbers, ticket_length);
 
