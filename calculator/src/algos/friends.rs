2d984b0f39cd3127b1aac19b837708d57d5432a6
sawyer bristol <sawyerbristol@gmail.com>
2023-09-12 22:23:12 +0000

    WIP - increase speed

--- calculator/src/algos/friends.rs
+++ calculator/src/algos/friends.rs
@@ -1,14 +1,15 @@
 use crate::LotteryTicket;
+// use itertools::Itertools;
 use std::collections::HashMap;
 
 pub fn friends(window: &[LotteryTicket], length: u8) -> Vec<u8> {
-    let mut friend_counter: HashMap<Vec<u8>, u32> = HashMap::new();
+    let mut friend_counter: HashMap<[u8; 2], u32> = HashMap::new();
 
     for ticket in window {
         for number1 in &ticket.numbers {
             for number2 in &ticket.numbers {
                 friend_counter
-                    .entry(vec![*number1, *number2])
+                    .entry([*number1, *number2])
                     .and_modify(|e| *e += 1)
                     .or_insert(1);
             }
@@ -16,25 +17,19 @@ pub fn friends(window: &[LotteryTicket], length: u8) -> Vec<u8> {
     }
 
     let mut output_vec = Vec::new();
-    let mut sort_vec = Vec::new();
-    for (friends, count) in friend_counter {
-        sort_vec.push((friends, count))
-    }
-    sort_vec.sort_by(|a, b| b.1.cmp(&a.1));
+    let mut output: Vec<([u8; 2], u32)> = friend_counter
+        .into_iter()
+        .map(|(friends, count)| (friends, count))
+        .collect::<Vec<([u8; 2], u32)>>();
+    output.sort_by(|a, b| b.1.cmp(&a.1));
+    output
+        .into_iter()
+        .filter(|(num, _)| num[0] != num[1])
+        .map(|(num, _)| {
+            output_vec.push(num[0]);
+            output_vec.push(num[1]);
+        });
 
-    for (num, _) in &sort_vec {
-        if num[0] != num[1] {
-            if output_vec.len() > length as usize {
-                output_vec.truncate(length as usize + 1);
-                break;
-            } else if num[0] != num[1] {
-                if !output_vec.contains(&num[0]) {
-                    output_vec.push(num[0]);
-                } else if !output_vec.contains(&num[1]) {
-                    output_vec.push(num[1]);
-                }
-            }
-        }
-    }
-    output_vec
+    output_vec.truncate(length as usize + 1);
+    output_vec //.unique().sort()
 }
