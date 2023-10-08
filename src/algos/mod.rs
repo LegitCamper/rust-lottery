511d29447cfbcd8fb5c1a38f2b947831a9cddb26
sawyer bristol <sawyerbristol@gmail.com>
2023-10-08 14:09:17 -0600

    further optimize optimize

--- src/algos/mod.rs
+++ src/algos/mod.rs
@@ -1,6 +1,8 @@
 use crate::{LotteryTicket, LotteryTickets};
 
+use chrono::NaiveDate;
 use combinations::Combinations;
+use std::cmp::Ordering;
 use tokio::task;
 
 mod friends;
@@ -23,45 +25,34 @@ pub async fn optimize(
 
     let max_depth: usize;
     if lottery_tickets.len() - 2 < WINDOW_SIZE {
+        // len - 2 to let last window have ticket to get accuracy
         max_depth = lottery_tickets.len() - 2
     } else {
         max_depth = WINDOW_SIZE;
     }
-    for w in 1..max_depth {
+    for window_size in 1..max_depth {
         tasks.push(task::spawn(async move {
             let mut weighted_matches: f64 = 0.0;
 
             let windows: std::slice::Windows<LotteryTicket> =
-                lottery_tickets[..lottery_tickets.len() - 1].windows(w);
+                lottery_tickets[..lottery_tickets.len() - 1].windows(window_size);
 
             for (window_index, window) in windows.clone().enumerate() {
                 let predicted_numbers = algo(window, ticket_size);
                 let predicted_tickets: Vec<Vec<u8>> =
                     Combinations::new(predicted_numbers, ticket_size.into()).collect();
 
-                // finds next ticket to compare for accuracy
-                #[allow(unused)]
-                let mut next_ticket = &LotteryTicket {
-                    date: chrono::NaiveDate::MIN,
-                    numbers: Vec::new(),
-                };
-                let mut lottery_tickets_iter = lottery_tickets.iter();
-                loop {
-                    let ticket = lottery_tickets_iter.next().unwrap();
-                    if ticket.date == window.last().unwrap().date {
-                        next_ticket = lottery_tickets_iter.next().unwrap();
-                        break;
-                    }
-                }
+                let next_ticket_index =
+                    find_tommorows_ticket(window.last().unwrap().date, &lottery_tickets[..])
+                        .unwrap();
 
                 // tally matching numbers and multiply weight - weight of recentness and weight of balls
                 let window_weight = 1.0 / window_index as f64;
-                println!("window_index: {window_index}"); // TODO: remvove once accuracy ensured
 
                 for ticket in predicted_tickets.iter() {
                     for num in ticket.iter() {
                         let ball_weight = num.pow(2) as f64;
-                        if next_ticket.numbers.contains(num) {
+                        if lottery_tickets[next_ticket_index].numbers.contains(num) {
                             weighted_matches += ball_weight * window_weight;
                         }
                     }
@@ -71,7 +62,7 @@ pub async fn optimize(
             // divide ball total by number of windows for fair eval later
             weighted_matches = weighted_matches / windows.len() as f64;
 
-            (w as u32, weighted_matches)
+            (window_size as u32, weighted_matches)
         }));
     }
 
@@ -85,4 +76,19 @@ pub async fn optimize(
 
     // returnes the optimal depth
     best_depth.0
+}
+
+fn find_tommorows_ticket(k: NaiveDate, items: &[LotteryTicket]) -> Option<usize> {
+    let mut low: usize = 0;
+    let mut high: usize = items.len();
+
+    while low < high {
+        let middle = (high + low) / 2;
+        match items[middle].date.cmp(&k) {
+            Ordering::Equal => return Some(middle),
+            Ordering::Greater => high = middle,
+            Ordering::Less => low = middle + 1,
+        }
+    }
+    None
 }
